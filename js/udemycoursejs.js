328ce3194ad094eed9c03464b30074978ffc4d21
rbtsoheib <taharsoheib08@gmail.com>
2024-08-11 19:34:05 +0200

    js / css / commits

--- js/udemycoursejs.js
+++ js/udemycoursejs.js
@@ -1,6 +1,100 @@
 
-var x = 300;
-var y; 
+// var x = 300;
+// var y; 
 
-x == 400 ? (y = "yes") : (y = "no");
-document.write(y);
+// x == 400 ? (y = "yes") : (y = "no");
+// document.write(y);
+
+// trying the switch statement
+
+
+ var date = 8 ;
+
+ switch (date) {
+
+    case 1:
+    
+            document.write(" <h1> today is monday </h1> <br> ");
+            document.write(" <h2> welcome </h2> ")
+
+        break;
+
+        case 2:
+
+            document.write("today is tuesday");
+        
+        break; 
+
+        case 3:
+
+            document.write("today is wedensday");
+        
+        break;    
+
+        case 4:
+
+            document.write("today is thursday");
+
+        break;    
+        
+        case 5:
+            
+            document.write("today is friday");
+        
+        break;    
+        
+        case 6:
+
+            document.write("today is saturday");
+        
+        break;
+ 
+    default:
+
+        document.write(" <h3> please enter the right number </h3> ");
+
+        break;
+ }
+
+    var sport = 7 ;
+
+    switch (sport) {
+
+        case 1:
+            
+                document.write(" <h3> ohhhh you like basketball, intresting ! </h3> ");
+
+            break;
+
+            case 2:
+
+                document.write("ohhhh you like football, intresting !");
+            
+            break;
+
+            case 3:
+
+                document.write("ohhhh you like tenis, intresting !");
+
+            break;
+
+            case 5:
+
+                document.write("ohhhh you like handball, intresting !");
+            
+            break;
+
+            case 6:
+
+                document.write("ohhhh you like swimming, intresting !");
+
+            break;
+    
+        default:
+
+            document.write(" <h3> please enter the right sport sir </h3> ");
+
+            break;
+    }
+
+    
